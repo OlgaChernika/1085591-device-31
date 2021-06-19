f53cc4f50bb2f982a47368bcaea22597bff8e8c4
Olga Chernikova <darkred2694@gmail.com>
2021-06-19 16:57:57 +0300

    Декоративные элементы на внутренней странице

--- js/script.js
+++ js/script.js
@@ -8,7 +8,7 @@ const clearActive = (controls, contents) => {
     contents.forEach((content) => {
       content.classList.remove("active");
     });
-}
+};
 
 const addActive = (controls, contents) => {
     controls.forEach((control, index) => {
@@ -28,3 +28,38 @@ if (tabBlock.length) {
       addActive(controls, contentBlocks);
     });
 };
+
+let writeUsLink = document.querySelector(".open-modal-write-us");
+let mapLink = document.querySelector(".open-modal-map");
+const modals = document.querySelectorAll(".modal");
+let popupWriteUs = document.querySelector(".modal-write-us");
+let popupMap = document.querySelector(".modal-map");
+let closeWriteUsButton = popupWriteUs.querySelector(".button-close");
+let closeMapButton = popupMap.querySelector(".button-close");
+
+const clearModalShow = (modals) => {
+  modals.forEach((modal) => {
+    modal.classList.remove("show");
+  });
+};
+
+writeUsLink.addEventListener("click", (link) => {
+    link.preventDefault();
+    clearModalShow(modals);
+    popupWriteUs.classList.add("show");
+});
+
+mapLink.addEventListener("click", (link) => {
+  link.preventDefault();
+  clearModalShow(modals);
+  popupMap.classList.add("show");
+});
+
+
+closeWriteUsButton.addEventListener("click", () => {
+  clearModalShow(modals);
+});
+
+closeMapButton.addEventListener("click", () => {
+  clearModalShow(modals);
+});
