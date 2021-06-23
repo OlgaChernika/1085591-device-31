bcd5c09b3c388f7747e5177cd00c909610f200e2
Olga Chernikova <darkred2694@gmail.com>
2021-06-23 19:18:16 +0300

    Доступная разметка внутренней страницы

--- js/script.js
+++ js/script.js
@@ -32,10 +32,13 @@ if (tabBlock.length) {
 const writeUsLink = document.querySelector(".open-modal-write-us");
 const mapLink = document.querySelector(".open-modal-map");
 const modals = document.querySelectorAll(".modal");
+const overlay = document.querySelector(".overlay");
 const popupWriteUs = document.querySelector(".modal-write-us");
 const popupMap = document.querySelector(".modal-map");
 const closeWriteUsButton = popupWriteUs.querySelector(".button-close");
 const closeMapButton = popupMap.querySelector(".button-close");
+const userNameInput = popupWriteUs.querySelector(".user-name-input");
+const mapIframe = popupMap.querySelector(".map-iframe");
 
 const clearModalShow = (modals) => {
   modals.forEach((modal) => {
@@ -43,23 +46,32 @@ const clearModalShow = (modals) => {
   });
 };
 
+const clearOverlay = (overlay) => {
+  overlay.classList.remove("show");
+};
+
 writeUsLink.addEventListener("click", (evt) => {
     evt.preventDefault();
     clearModalShow(modals);
+    overlay.classList.add("show");
     popupWriteUs.classList.add("show");
+    userNameInput.focus();
 });
 
 mapLink.addEventListener("click", (evt) => {
   evt.preventDefault();
   clearModalShow(modals);
+  overlay.classList.add("show");
   popupMap.classList.add("show");
+  mapIframe.focus();
 });
-
 
 closeWriteUsButton.addEventListener("click", () => {
   clearModalShow(modals);
+  clearOverlay(overlay);
 });
 
 closeMapButton.addEventListener("click", () => {
   clearModalShow(modals);
+  clearOverlay(overlay);
 });
